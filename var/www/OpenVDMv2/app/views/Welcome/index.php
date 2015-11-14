83d6dba4aa9fcb5a0c315e871aca399221cd6ac2
Webb Pinner <webbpinner@gmail.com>
2015-11-14 10:08:13 -0500

    Update index.php

--- var/www/OpenVDMv2/app/views/Welcome/index.php
+++ var/www/OpenVDMv2/app/views/Welcome/index.php
@@ -101,7 +101,7 @@ foreach($data['requiredCruiseDataTransfers'] as $row){
         
         for($i = count($data['shipToShoreTransfers'])-1; $i >= 0; $i-- ) {
 ?>
-                    <h5><?php echo $data['shipToShoreTransfers'][$i]->shipToShoreTransferName; ?> - <?php $timestamp = DateTime::createFromFormat('Ymd\THis\Z', $data['shipToShoreTransfers'][$i]->date, new DateTimeZone('UTC')); echo $timestamp->format('Y-m-d H:i:s T'); ?></h5>
+                    <h5>SSDW - <?php $timestamp = DateTime::createFromFormat('Ymd\THis\Z', $data['shipToShoreTransfers'][$i]->date, new DateTimeZone('UTC')); echo $timestamp->format('Y-m-d H:i:s T'); ?></h5>
                     <ul>
 <?php
             foreach($data['shipToShoreTransfers'][$i]->newFiles as $file) {
