bf43c737f9168dd39dfe5c5439555d18ccb92da1
Webb Pinner <webbpinner@gmail.com>
2017-10-05 10:10:11 -0400

    Update OpenVDMv2_db.sql

--- OpenVDMv2_db.sql
+++ OpenVDMv2_db.sql
@@ -16,17 +16,17 @@ CREATE TABLE `ODVM_Status` (
   `statusID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `status` tinytext,
   PRIMARY KEY (`statusID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `ODVM_Status` WRITE;
 /*!40000 ALTER TABLE `ODVM_Status` DISABLE KEYS */;
 
 INSERT INTO `ODVM_Status` (`statusID`, `status`)
 VALUES
-  (1,'Running'),
-  (2,'Idle'),
-  (3,'Error'),
-  (4,'Off');
+	(1,'Running'),
+	(2,'Idle'),
+	(3,'Error'),
+	(4,'Off');
 
 /*!40000 ALTER TABLE `ODVM_Status` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -71,7 +71,8 @@ CREATE TABLE `OVDM_CollectionSystemTransfers` (
   KEY `CollectionSystemTransferType` (`transferType`),
   CONSTRAINT `CollectionSystemTransferStatus` FOREIGN KEY (`status`) REFERENCES `ODVM_Status` (`statusID`),
   CONSTRAINT `CollectionSystemTransferType` FOREIGN KEY (`transferType`) REFERENCES `OVDM_TransferTypes` (`transferTypeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 # Dump of table OVDM_CoreVars
 # ------------------------------------------------------------
@@ -83,32 +84,32 @@ CREATE TABLE `OVDM_CoreVars` (
   `name` tinytext NOT NULL,
   `value` tinytext,
   PRIMARY KEY (`coreVarID`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_CoreVars` WRITE;
 /*!40000 ALTER TABLE `OVDM_CoreVars` DISABLE KEYS */;
 
 INSERT INTO `OVDM_CoreVars` (`coreVarID`, `name`, `value`)
 VALUES
-  (1,'shipboardDataWarehouseIP','127.0.0.1'),
-  (2,'shipboardDataWarehouseUsername','survey'),
-  (3,'shipboardDataWarehousePublicDataDir','/vault/FTPRoot/PublicData'),
-  (4,'shipboardDataWarehouseStatus','2'),
-  (5,'cruiseID','CS1701'),
-  (6,'cruiseStartDate','2017/01/01 00:00'),
-  (7,'cruiseEndDate',''),
-  (8,'cruiseSize','0'),
-  (9,'cruiseSizeUpdated','1970/01/01 00:00:00'),
-  (10,'loweringID',''),
-  (11,'loweringStartDate',''),
-  (12,'loweringEndDate',''),
-  (13,'loweringSize','0'),
-  (14,'loweringSizeUpdated','1970/01/01 00:00:00'),
-  (15,'systemStatus','Off'),
-  (16,'shipToShoreBWLimitStatus','Off'),
-  (17,'md5FilesizeLimit','10'),
-  (18,'md5FilesizeLimitStatus','On'),
-  (19,'showLoweringComponents','No');
+	(1,'shipboardDataWarehouseIP','127.0.0.1'),
+	(2,'shipboardDataWarehouseUsername','survey'),
+	(3,'shipboardDataWarehousePublicDataDir','/vault/FTPRoot/PublicData'),
+	(4,'shipboardDataWarehouseStatus','2'),
+	(5,'cruiseID','CS1701'),
+	(6,'cruiseStartDate','2017/10/05 00:00'),
+	(7,'cruiseEndDate',''),
+	(8,'cruiseSize','0'),
+	(9,'cruiseSizeUpdated','2017/10/05 00:00'),
+	(10,'loweringID',''),
+	(11,'loweringStartDate',''),
+	(12,'loweringEndDate',''),
+	(13,'loweringSize','0'),
+	(14,'loweringSizeUpdated','2017/10/05 00:00'),
+	(15,'systemStatus','Off'),
+	(16,'shipToShoreBWLimitStatus','Off'),
+	(17,'md5FilesizeLimit','10'),
+	(18,'md5FilesizeLimitStatus','On'),
+	(19,'showLoweringComponents','No');
 
 /*!40000 ALTER TABLE `OVDM_CoreVars` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -143,19 +144,21 @@ CREATE TABLE `OVDM_CruiseDataTransfers` (
   `pid` int(11) unsigned DEFAULT '0',
   `bandwidthLimit` int(10) unsigned NOT NULL DEFAULT '0',
   `includeOVDMFiles` int(1) unsigned NOT NULL DEFAULT '0',
+  `excludedCollectionSystems` tinytext,
+  `excludedExtraDirectories` tinytext,
   PRIMARY KEY (`cruiseDataTransferID`),
   KEY `CruiseDataTransferStatus` (`status`),
   KEY `CruiseDataTransferType` (`transferType`),
   CONSTRAINT `CruiseDataTransferStatus` FOREIGN KEY (`status`) REFERENCES `ODVM_Status` (`statusID`),
   CONSTRAINT `CruiseDataTransferType` FOREIGN KEY (`transferType`) REFERENCES `OVDM_TransferTypes` (`transferTypeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_CruiseDataTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_CruiseDataTransfers` DISABLE KEYS */;
 
-INSERT INTO `OVDM_CruiseDataTransfers` (`cruiseDataTransferID`, `name`, `longName`, `transferType`, `destDir`, `localDirIsMountPoint`, `rsyncServer`, `rsyncUser`, `rsyncPass`, `smbServer`, `smbUser`, `smbPass`, `smbDomain`, `sshServer`, `sshUser`, `sshUseKey`, `sshPass`, `status`, `enable`, `required`, `pid`, `bandwidthLimit`, `includeOVDMFiles`)
+INSERT INTO `OVDM_CruiseDataTransfers` (`cruiseDataTransferID`, `name`, `longName`, `transferType`, `destDir`, `localDirIsMountPoint`, `rsyncServer`, `rsyncUser`, `rsyncPass`, `smbServer`, `smbUser`, `smbPass`, `smbDomain`, `sshServer`, `sshUser`, `sshUseKey`, `sshPass`, `status`, `enable`, `required`, `pid`, `bandwidthLimit`, `includeOVDMFiles`, `excludedCollectionSystems`, `excludedExtraDirectories`)
 VALUES
-  (1,'SSDW','Shoreside Data Warehouse',4,'/vault/Shoreside',0,'','','','','','','','127.0.0.1','survey',0,'Tethys337813',2,1,1,0,128,0);
+	(1,'SSDW','Shoreside Data Warehouse',4,'/vault/Shoreside',0,'','','','','','','','127.0.0.1','survey',0,'password',2,1,1,0,128,0,'0','0'),
 
 /*!40000 ALTER TABLE `OVDM_CruiseDataTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -174,16 +177,16 @@ CREATE TABLE `OVDM_ExtraDirectories` (
   `enable` tinyint(1) DEFAULT '0',
   `required` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`extraDirectoryID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_ExtraDirectories` WRITE;
 /*!40000 ALTER TABLE `OVDM_ExtraDirectories` DISABLE KEYS */;
 
 INSERT INTO `OVDM_ExtraDirectories` (`extraDirectoryID`, `name`, `longName`, `destDir`, `enable`, `required`)
 VALUES
-  (1,'Transfer Logs','Transfer Logs','OpenVDM/TransferLogs',1,1),
-  (2,'Dashboard Data','Dashboard Data','OpenVDM/DashboardData',1,1),
-  (3,'Science','Misc. cruise docs, pictures and data. ','Science',1,1);
+	(1,'Transfer Logs','Transfer Logs','OpenVDM/TransferLogs',1,1),
+	(2,'Dashboard Data','Dashboard Data','OpenVDM/DashboardData',1,1),
+	(3,'Science','Misc. cruise docs, pictures and data. ','Science',1,1);
 
 /*!40000 ALTER TABLE `OVDM_ExtraDirectories` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -204,7 +207,7 @@ CREATE TABLE `OVDM_Gearman` (
   `jobName` tinytext,
   `jobPid` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`jobID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
@@ -220,19 +223,19 @@ CREATE TABLE `OVDM_Links` (
   `enable` tinyint(1) NOT NULL DEFAULT '0',
   `private` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`linkID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_Links` WRITE;
 /*!40000 ALTER TABLE `OVDM_Links` DISABLE KEYS */;
 
 INSERT INTO `OVDM_Links` (`linkID`, `name`, `url`, `enable`, `private`)
 VALUES
-  (1,'Supervisord','http://{hostIP}:9001',1,1),
-  (2,'Gearman','http://{hostIP}/gearman-ui/',1,1),
-  (3,'Cruise Data','http://{hostIP}/CruiseData/{cruiseID}/',1,0),
-  (4,'Public Data','http://{hostIP}/PublicData/',1,0),
-  (5,'Visitor Information','http://{hostIP}/VisitorInformation/',1,0),
-  (6,'MapProxy','http://{hostIP}/mapproxy/demo/',1,0);
+	(1,'Supervisord','http://{hostIP}:9001',1,1),
+	(2,'Gearman','http://{hostIP}/gearman-ui/',1,1),
+	(3,'Cruise Data','http://{hostIP}/CruiseData/{cruiseID}/',1,0),
+	(4,'Public Data','http://{hostIP}/PublicData/',1,0),
+	(5,'Visitor Information','http://{hostIP}/VisitorInformation/',1,0),
+	(6,'MapProxy','http://{hostIP}/mapproxy/demo/',1,0);
 
 /*!40000 ALTER TABLE `OVDM_Links` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -250,7 +253,7 @@ CREATE TABLE `OVDM_Messages` (
   `messageTS` datetime NOT NULL,
   `messageViewed` tinyint(1) NOT NULL,
   PRIMARY KEY (`messageID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 # Dump of table OVDM_ShipToShoreTransfers
@@ -269,17 +272,17 @@ CREATE TABLE `OVDM_ShipToShoreTransfers` (
   `enable` tinyint(1) NOT NULL DEFAULT '0',
   `required` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`shipToShoreTransferID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_ShipToShoreTransfers` WRITE;
 /*!40000 ALTER TABLE `OVDM_ShipToShoreTransfers` DISABLE KEYS */;
 
 INSERT INTO `OVDM_ShipToShoreTransfers` (`shipToShoreTransferID`, `name`, `longName`, `priority`, `collectionSystem`, `extraDirectory`, `includeFilter`, `enable`, `required`)
 VALUES
-  (1,'DashboardData','Dashboard Data',1,0,2,'*',1,1),
-  (2,'TransferLogs','Transfer Logs',1,0,1,'*',0,1),
-  (3,'MD5Summary','MD5 Summary',1,0,0,'MD5_Summary.txt MD5_Summary.md5 ',1,1),
-  (4,'OVDM_Config','OpenVDM Configuration',1,0,0,'ovdmConfig.json',1,1);
+	(1,'DashboardData','Dashboard Data',1,0,2,'*',1,1),
+	(2,'TransferLogs','Transfer Logs',1,0,1,'*',0,1),
+	(3,'MD5Summary','MD5 Summary',1,0,0,'MD5_Summary.txt MD5_Summary.md5 ',1,1),
+	(4,'OVDM_Config','OpenVDM Configuration',1,0,0,'ovdmConfig.json',1,1),
 
 /*!40000 ALTER TABLE `OVDM_ShipToShoreTransfers` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -301,24 +304,24 @@ CREATE TABLE `OVDM_Tasks` (
   PRIMARY KEY (`taskID`),
   KEY `ProcessStatus` (`status`),
   CONSTRAINT `ProcessStatus` FOREIGN KEY (`status`) REFERENCES `ODVM_Status` (`statusID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_Tasks` WRITE;
 /*!40000 ALTER TABLE `OVDM_Tasks` DISABLE KEYS */;
 
 INSERT INTO `OVDM_Tasks` (`taskID`, `name`, `longName`, `cruiseOrLowering`, `status`, `enable`, `pid`)
 VALUES
-  (1,'setupNewCruise','Setup New Cruise',0,2,1,0),
-  (2,'finalizeCurrentCruise','Finalize Current Cruise',0,2,1,0),
-  (3,'rebuildMD5Summary','Rebuild MD5 Summary',0,2,1,0),
-  (4,'rebuildDataDashboard','Rebuild Data Dashboard',0,2,1,0),
-  (5,'rebuildCruiseDirectory','Rebuild Cruise Directory',0,2,1,0),
-  (6,'exportOVDMConfig','Re-export the OpenVDM Configuration',0,2,1,0),
-  (7,'rsyncPublicDataToCruiseData','Sync PublicData to the Science Folder within current Cruise Directory',0,2,1,0),
-  (8,'setupNewLowering','Setup New Lowering',1,2,1,0),
-  (9,'finalizeCurrentLowering','Finalize Current Lowering',1,2,1,0),
-  (10,'rebuildLoweringDirectory','Rebuild Lowering Directory',1,2,1,0),
-  (11,'exportLoweringConfig','Re-export the Lowering Configuration',1,2,1,0);
+	(1,'setupNewCruise','Setup New Cruise',0,2,1,0),
+	(2,'finalizeCurrentCruise','Finalize Current Cruise',0,2,1,0),
+	(3,'rebuildMD5Summary','Rebuild MD5 Summary',0,2,1,0),
+	(4,'rebuildDataDashboard','Rebuild Data Dashboard',0,2,1,0),
+	(5,'rebuildCruiseDirectory','Rebuild Cruise Directory',0,2,1,0),
+	(6,'exportOVDMConfig','Re-export the OpenVDM Configuration',0,2,1,0),
+	(7,'rsyncPublicDataToCruiseData','Sync PublicData to the Science Folder within current Cruise Directory',0,2,1,0),
+	(8,'setupNewLowering','Setup New Lowering',1,2,1,0),
+	(9,'finalizeCurrentLowering','Finalize Current Lowering',1,2,1,0),
+	(10,'rebuildLoweringDirectory','Rebuild Lowering Directory',1,2,1,0),
+	(11,'exportLoweringConfig','Re-export the Lowering Configuration',1,2,1,0);
 
 /*!40000 ALTER TABLE `OVDM_Tasks` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -333,17 +336,17 @@ CREATE TABLE `OVDM_TransferTypes` (
   `transferTypeID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `transferType` tinytext,
   PRIMARY KEY (`transferTypeID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_TransferTypes` WRITE;
 /*!40000 ALTER TABLE `OVDM_TransferTypes` DISABLE KEYS */;
 
 INSERT INTO `OVDM_TransferTypes` (`transferTypeID`, `transferType`)
 VALUES
-  (1,'Local Directory'),
-  (2,'Rsync Server'),
-  (3,'SMB Share'),
-  (4,'SSH Server');
+	(1,'Local Directory'),
+	(2,'Rsync Server'),
+	(3,'SMB Share'),
+	(4,'SSH Server');
 
 /*!40000 ALTER TABLE `OVDM_TransferTypes` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -360,14 +363,14 @@ CREATE TABLE `OVDM_Users` (
   `password` varchar(255) DEFAULT '',
   `lastLogin` datetime DEFAULT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `OVDM_Users` WRITE;
 /*!40000 ALTER TABLE `OVDM_Users` DISABLE KEYS */;
 
 INSERT INTO `OVDM_Users` (`userID`, `username`, `password`, `lastLogin`)
 VALUES
-  (1,'admin','$2y$12$JviETOQPkNzqZxQpswLb1ONtTLxsqdzQJEoaWjlNzb0/.xfIOVM/C','2017-10-03 10:51:37');
+	(1,'admin','$2y$12$JviETOQPkNzqZxQpswLb1ONtTLxsqdzQJEoaWjlNzb0/.xfIOVM/C','2017-10-05 10:47:49');
 
 /*!40000 ALTER TABLE `OVDM_Users` ENABLE KEYS */;
 UNLOCK TABLES;
