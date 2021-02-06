d82bed7a03eca5ae0515f62ef3c6765dd4888c3b
Webb Pinner <oceandatarat@gmail.com>
2021-02-06 15:18:17 -0500

    Removed un-needed files

--- utils/scripts/OpenVDMv2-restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp ../backup/usr/local/etc/openvdm/datadashboard.yaml /usr/local/etc/openvdm/
-cp ../backup/usr/local/etc/openvdm/openvdm.yaml /usr/local/etc/openvdm/
-
-cp ../backup/var/www/OpenVDMv2/.htaccess /var/www/OpenVDMv2/
-cp ../backup/var/www/OpenVDMv2/app/Core/Config.php /var/www/OpenVDMv2/app/Core/
-
